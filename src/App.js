be16a20399acb0222cb09d3e6664958c57c2188f
kumaran47675 <133028937+kumaran47675@users.noreply.github.com>
2023-09-22 12:36:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
-// App.js
-import React, { useState } from 'react';
-import {  BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Observation from './Components/Observation';
-import Landing from './Components/Landing';
-import Tinting from './Components/Tinting-Banco';
-import Login from './Components/Login';
+import ApprovalFormat from "./ApprovalFormat";
+import LoginPage from "./Login";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import {useState} from 'react';
 
 function App() {
-  const [token, setToken] = useState(false);
-  const [user,setUser]=useState(" ");
-  return (
-    <Router>
-        <>
-          <Header />
-          <Routes>
-          
-            <Route path="/" element={token ? <Landing /> : <Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/observation" element={token ?  <Observation />:<Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/landing" element={token ? <Landing username={user} />:<Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/tinting" element={token ?  <Tinting />:<Login setToken={setToken} setUser={setUser} />} />
-          </Routes>
-        </>
+    const [token, setToken] = useState(false);
+    const [user,setUser]=useState("");
+
+
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={token ? <ApprovalFormat /> : <LoginPage setToken={setToken} setUser={setUser} />} />
+                <Route exact path="/approvalformat" element= {token ? <ApprovalFormat Username={user}/>:<LoginPage setToken={setToken} setUser={setUser} />} />
+            </Routes>
         </Router>
-  );
+    )
 }
 
 export default App;
-
