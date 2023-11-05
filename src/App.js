a9399c6c51e29877caf8da93cb8d0141a8b9b7f5
kumaran47675 <133028937+kumaran47675@users.noreply.github.com>
2023-11-05 22:55:14 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-// App.js
-import React, { useState } from 'react';
-import {  BrowserRouter as Router,Route, Routes } from 'react-router-dom';
-import Header from './Components/Header';
-import Observation from './Components/Observation';
-import Landing from './Components/Landing';
-import Tinting from './Components/Tinting-Banco';
-import Login from './Components/Login';
-import "./App.css"
+import MainPage from './Components/MainPage';
+import './App.css';
+
 function App() {
-  const [token, setToken] = useState(false);
-  const [user,setUser]=useState("");
   return (
-    <Router>
-        <>
-          <Header />
-          <Routes>
-          
-            <Route path="/" element={token ? <Landing /> : <Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/observation" element={token ?  <Observation username={user} />:<Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/landing" element={token ? <Landing username={user} />:<Login setToken={setToken} setUser={setUser} />} />
-            <Route path="/tinting" element={token ?  <Tinting />:<Login setToken={setToken} setUser={setUser} />} />
-          </Routes>
-        </>
-        </Router>
+    <div className="App">
+      <MainPage/>
+    </div>
   );
 }
 
 export default App;
+
+
